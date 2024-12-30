a143e3ea4d042f5329841e1fa288aa1e8d64c3d5
olaniyihope2 <olaniyihope2@gmail.com>
2024-12-30 10:40:47 +0100

    all

--- src/routers/matchRoute.js
+++ src/routers/matchRoute.js
@@ -5,21 +5,24 @@ const {
     getLastUploadedMatchedItemsForSwitch,
     getLastUploadedUnmatchedItemsForSwitch,
 } = require('../controllers/matchController')
+const authenticateUser = require('../middlewares/authenticateUser')
 
 const router = express.Router()
 
 // Save matched items
-router.post('/matches', saveMatchedItems)
+router.post('/matches', authenticateUser, saveMatchedItems)
 
 // Get matched items by switchId
-router.get('/matches/:switchId', getMatchedItems)
+router.get('/matches/:switchId', authenticateUser, getMatchedItems)
 router.get(
     '/matches/last-uploaded/:switchId',
+    authenticateUser,
     getLastUploadedMatchedItemsForSwitch
 )
 
 router.get(
     '/unmatched/last-uploaded/:switchId',
+    authenticateUser,
     getLastUploadedUnmatchedItemsForSwitch
 )
 
